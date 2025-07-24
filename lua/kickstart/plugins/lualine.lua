c7f324519f96758bb884d7437eb644126bee8624
iacobo <47821203+iacobo@users.noreply.github.com>
2025-07-24 09:56:41 +0100

    Setup lualine.

--- lua/kickstart/plugins/lualine.lua
+++ lua/kickstart/plugins/lualine.lua
@@ -1,14 +1,10 @@
--- NOTE: Plugins can specify dependencies.
---
--- The dependencies are proper plugin specifications as well - anything
--- you do for a plugin at the top level, you can do for a dependency.
---
--- Use the `dependencies` key to specify the dependencies of a particular plugin
-
 return {
   {
     'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' }
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+        require('lualine').setup {
+        }
+    end
   }
 }
--- vim: ts=2 sts=2 sw=2 et
