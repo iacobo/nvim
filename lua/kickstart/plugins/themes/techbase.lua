9e47490277de6517e5e9f60f4c2316a716a61936
iacobo <47821203+iacobo@users.noreply.github.com>
2025-07-27 07:13:15 +0100

    Add plugins.

---
+++ lua/kickstart/plugins/themes/techbase.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        'mcauley-penney/techbase.nvim',
+        config = function()
+            vim.cmd.colorscheme('techbase')
+        end,
+        priority = 1000
+    }
+}
+-- vim: ts=2 sts=2 sw=2 et
