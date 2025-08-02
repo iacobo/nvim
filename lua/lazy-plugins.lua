3e9713356b50d7480ee3be4b292f35f6c3c18458
iacobo <47821203+iacobo@users.noreply.github.com>
2025-08-02 14:53:00 +0100

    Add Typr.

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -43,6 +43,7 @@ require('lazy').setup({
     require 'kickstart.plugins.treesitter',
     require 'kickstart.plugins.themery',
     require 'kickstart.plugins.themes',
+    require 'kickstart.plugins.typr',
 
     -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
     -- init.lua. If you want these files, they are in the repository, so you can just download them and
